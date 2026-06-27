1c20e0b97e7a2757d4c3d72de87f60fd36bf0aed
SameerRathore10 <sameerrathore803@gmail.com>
2026-06-27 20:14:07 +0530

    Fix map marker icon

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -6,6 +6,14 @@ import { useNavigate } from "react-router-dom";
 import { db } from "../../firebase/firebase";
 
 import "leaflet/dist/leaflet.css";
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 const MapPage = () => {
   const [reports, setReports] = useState([]);
